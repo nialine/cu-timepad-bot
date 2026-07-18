52b9b9a0f85ee11c9ae85c49045d383400537236
Андрей Мишурин <mishurin4@gmail.com>
2026-07-18 10:50:13 +0300

    feat(middleware): add singleflight and autorespond middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"context"
 	"log/slog"
+	"sync"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -10,13 +11,41 @@ import (
 
 func Logging(next bot.HandlerFunc) bot.HandlerFunc {
 	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
-		slog.LogAttrs(ctx,
-			slog.LevelDebug,
-			"Got message",
-			slog.Int64("chatid", update.Message.Chat.ID),
-			slog.String("message", update.Message.Text),
-		)
+		if update.Message != nil {
+			slog.LogAttrs(ctx,
+				slog.LevelDebug,
+				"Got message",
+				slog.Int64("chatid", update.Message.Chat.ID),
+				slog.String("message", update.Message.Text),
+			)
+		}
 		next(ctx, b, update)
+	}
+}
 
+func AutoRespond(next bot.HandlerFunc) bot.HandlerFunc {
+	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
+		if update.Message != nil {
+			b.SendChatAction(ctx, &bot.SendChatActionParams{
+				ChatID: update.Message.Chat.ID,
+				Action: models.ChatActionTyping,
+			})
+		}
+		next(ctx, b, update)
+	}
+}
+
+func SingleFlight(next bot.HandlerFunc) bot.HandlerFunc {
+	sf := sync.Map{}
+
+	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
+		if update.CallbackQuery != nil {
+			key := update.CallbackQuery.ID
+			if _, loaded := sf.LoadOrStore(key, nil); loaded {
+				return
+			}
+			defer sf.Delete(key)
+		}
+		next(ctx, b, update)
 	}
 }
